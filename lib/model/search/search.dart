94a6820ff337b4827840c55adba2977e1e91c22c
Julian Aryo <julian.aryo1989@gmail.com>
2020-11-23 15:04:34 +0800

    refactor search model

--- lib/model/search/search.dart
+++ lib/model/search/search.dart
@@ -5,44 +5,20 @@ part 'search.g.dart';
 @JsonSerializable()
 class Search {
   Search({
-    this.id,
-    this.searchId,
-    this.userId,
     this.keyword,
     this.hits,
-    this.createdAt,
-    this.updatedAt,
   });
 
-  final int id;
-  @JsonKey(name: 'search_id')
-  final String searchId;
-  @JsonKey(name: 'user_id')
-  final String userId;
   final String keyword;
   final int hits;
-  @JsonKey(name: 'created_at')
-  final DateTime createdAt;
-  @JsonKey(name: 'updated_at')
-  final DateTime updatedAt;
 
   Search copyWith({
-    int id,
-    String searchId,
-    String userId,
     String keyword,
     int hits,
-    DateTime createdAt,
-    DateTime updatedAt,
   }) =>
       Search(
-        id: id ?? this.id,
-        searchId: searchId ?? this.searchId,
-        userId: userId ?? this.userId,
         keyword: keyword ?? this.keyword,
         hits: hits ?? this.hits,
-        createdAt: createdAt ?? this.createdAt,
-        updatedAt: updatedAt ?? this.updatedAt,
       );
 
   factory Search.fromJson(Map<String, dynamic> json) => _$SearchFromJson(json);
